0d14946d27bbe7719bfc45606cb8b85fe7e51e13
Joe Conley <joe.conley@lairdtech.com>
2016-04-22 16:18:02 -0400

    WebLCM: Add support for sending modified basic profile properties to PHP_SDK

---
+++ php/setProfile.php
@@ -0,0 +1,33 @@
+<?php
+	require("../lrd_php_sdk.php");
+	if(!extension_loaded('lrd_php_sdk')){
+		syslog(LOG_WARNING, "ERROR: failed to load lrd_php_sdk");
+	}
+	header("Content-Type: application/json");
+	$Profile = json_decode(stripslashes(file_get_contents("php://input")));
+
+	$returnedResult = [
+		'SDCERR' => 1,
+	];
+
+	$cfgs = new SDCConfig();
+	$result = GetConfig($Profile->{'profileName'},$cfgs);
+
+	$cfgs->SSID = $Profile->{'SSID'};
+	$cfgs->clientName = $Profile->{'clientName'};
+	$cfgs->txPower = $Profile->{'txPower'};
+	$cfgs->authType = $Profile->{'authType'};
+	$cfgs->eapType = $Profile->{'eapType'};
+	$cfgs->powerSave = ($Profile->{'powerSave'} & 0xf);
+	$cfgs->wepType = $Profile->{'wepType'};
+	$cfgs->bitRate = $Profile->{'bitRate'};
+	$cfgs->radioMode = $Profile->{'radioMode'};
+
+	$result = ModifyConfig(trim($Profile->{'profileName'}),$cfgs);
+
+	$returnedResult['SDCERR'] = $result;
+
+	echo json_encode($returnedResult);
+
+	free_SDCConfig($cfgs);
+?>
