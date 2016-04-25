26626f80f2c9fe0b804b4a55cb8ac7d2f063f036
Joe Conley <joe.conley@lairdtech.com>
2016-04-25 10:51:22 -0400

    WebLCM: Set default return type for adding a profile to fail

--- php/addProfile.php
+++ php/addProfile.php
@@ -1,10 +1,14 @@
 <?php
-	include("../lrd_php_sdk.php");
+	require("../lrd_php_sdk.php");
 	if(!extension_loaded('lrd_php_sdk')){
 		syslog(LOG_WARNING, "ERROR: failed to load lrd_php_sdk");
 	}
 	header("Content-Type: application/json");
 	$newProfile = json_decode(stripslashes(file_get_contents("php://input")));
+
+	$returnedResult = [
+		'SDCERR' => SDCERR_FAIL,
+	];
 
 	$cfgs = new SDCConfig();
 
@@ -37,9 +41,7 @@
 		$result = AddConfig($cfgs);
 	}
 
-	$returnedResult = [
-		'SDCERR' => $result,
-	];
+	$returnedResult['SDCERR'] = $result;
 
 	echo json_encode($returnedResult);
 
