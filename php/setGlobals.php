8cba04efec37d2fe010f8aae68f8b7b72c6ca0fb
Joe Conley <joe.conley@lairdtech.com>
2016-05-16 17:32:55 -0400

    WebLCM: Add full support for setting global settings
    All supported global settings can be set though user interface. A check has
    been added at submission time to verify settings are supported. This means there
    is a check for supported options when items are displayed as well as when
    options are attempted to be submitted.

--- php/setGlobals.php
+++ php/setGlobals.php
@@ -9,15 +9,117 @@
 	$returnedResult = [
 		'SDCERR' => SDCERR_FAIL,
 	];
-
-	$cfgs = new SDCGlobalConfig();
-	$result = GetGlobalSettings($cfgs);
-
+	$rcs = new_RADIOCHIPSETp();
+	$result = LRD_WF_GetRadioChipSet($rcs);
 	if($result == SDCERR_SUCCESS){
-		$cfgs->authServerType = $global->{'authServerType'};
 
-		$result = SetGlobalSettings($cfgs);
+		$supportedGlobals = new SDCGlobalConfig();
+		$result = LRD_WF_GetSupportedGlobals(RADIOCHIPSETp_value($rcs), $supportedGlobals);
+		if($result == SDCERR_SUCCESS){
+			$cfgs = new SDCGlobalConfig();
+			$result = GetGlobalSettings($cfgs);
+
+			if($result == SDCERR_SUCCESS){
+				if ($supportedGlobals->aLRS){
+					$cfgs->aLRS = $global->{'aLRS'};
+				}
+
+				if ($supportedGlobals->authServerType){
+					$cfgs->authServerType = $global->{'authServerType'};
+				}
+
+				if ($supportedGlobals->bLRS){
+					$cfgs->bLRS = $global->{'bLRS'};
+				}
+
+				if ($supportedGlobals->BeaconMissTimeout){
+					$cfgs->BeaconMissTimeout = $global->{'BeaconMissTimeout'};
+				}
+
+				if ($supportedGlobals->BTcoexist){
+					$cfgs->BTcoexist = $global->{'BTcoexist'};
+				}
+
+				if ($supportedGlobals->CCXfeatures){
+					$cfgs->CCXfeatures = $global->{'CCXfeatures'};
+				}
+
+				if ($supportedGlobals->certPath){
+					$cfgs->certPath = $global->{'certPath'};
+				}
+
+				if ($supportedGlobals->defAdhocChannel){
+					$cfgs->defAdhocChannel = $global->{'defAdhocChannel'};
+				}
+
+				if ($supportedGlobals->DFSchannels){
+					$cfgs->DFSchannels = $global->{'DFSchannels'};
+				}
+
+				if ($supportedGlobals->fragThreshold){
+					$cfgs->fragThreshold = $global->{'fragThreshold'};
+				}
+
+				if ($supportedGlobals->PMKcaching){
+					$cfgs->PMKcaching = $global->{'PMKcaching'};
+				}
+
+				if ($supportedGlobals->probeDelay){
+					$cfgs->probeDelay = $global->{'probeDelay'};
+				}
+
+				if ($supportedGlobals->regDomain){
+					$cfgs->regDomain = $global->{'regDomain'};
+				}
+
+				if ($supportedGlobals->roamPeriodms){
+					$cfgs->roamPeriodms = $global->{'roamPeriodms'};
+				}
+
+				if ($supportedGlobals->roamTrigger){
+					$cfgs->roamTrigger = $global->{'roamTrigger'};
+				}
+
+				if ($supportedGlobals->RTSThreshold){
+					$cfgs->RTSThreshold = $global->{'RTSThreshold'};
+				}
+
+				if ($supportedGlobals->scanDFSTime){
+					$cfgs->scanDFSTime = $global->{'scanDFSTime'};
+				}
+
+				if ($supportedGlobals->TTLSInnerMethod){
+					$cfgs->TTLSInnerMethod = $global->{'TTLSInnerMethod'};
+				}
+
+				if ($supportedGlobals->uAPSD){
+					$cfgs->uAPSD = $global->{'uAPSD'};
+				}
+
+				if ($supportedGlobals->WMEenabled){
+					$cfgs->WMEenabled = $global->{'WMEenabled'};
+				}
+
+				if ($supportedGlobals->suppInfo & 4){
+					if ($global->{'suppInfoDateCheck'} == 1){
+						$cfgs->suppInfo |= SUPPINFO_TLS_TIME_CHECK;
+					}elseif ($global->{'suppInfoDateCheck'} == 0){
+						$cfgs->suppInfo &= ~SUPPINFO_TLS_TIME_CHECK;
+					}
+				}
+				if ($supportedGlobals->suppInfo & 1){
+					if ($global->{'fips'} == 1){
+						$cfgs->suppInfo |= SUPPINFO_FIPS;
+					}elseif ($global->{'fips'} == 0){
+						$cfgs->suppInfo &= ~SUPPINFO_FIPS;
+					}
+				}
+
+				$result = SetGlobalSettings($cfgs);
+			}
+		}
 	}
+	delete_RADIOCHIPSETp($rcs);
 
 	$returnedResult['SDCERR'] = $result;
 
