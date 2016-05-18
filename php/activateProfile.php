2dfa6da682bbcdc04dbeceeee535d0ed9aca6cd4
Joe Conley <joe.conley@lairdtech.com>
2016-05-18 10:59:00 -0400

    WebLCM: Add ability to set the active profile
    Now a user can activate profiles.
    The current active profile will automatically be selected in the list.
    The help text will also list the active profile.

---
+++ php/activateProfile.php
@@ -0,0 +1,18 @@
+<?php
+	require("../lrd_php_sdk.php");
+	if(!extension_loaded('lrd_php_sdk')){
+		syslog(LOG_WARNING, "ERROR: failed to load lrd_php_sdk");
+	}
+	header("Content-Type: application/json");
+	$Profile = json_decode(stripslashes(file_get_contents("php://input")));
+
+	$returnedResult = [
+		'SDCERR' => SDCERR_FAIL,
+	];
+
+	$result = ActivateConfig($Profile->profileName);
+
+	$returnedResult['SDCERR'] = $result;
+
+	echo json_encode($returnedResult);
+?>
