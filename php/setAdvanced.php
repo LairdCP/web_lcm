9a45e3933e5cc888d2f506d490de9499a4387589
Joe Conley <joe.conley@lairdtech.com>
2016-05-19 15:51:12 -0400

    WebLCM: Add ability to set supplicant/driver debug levels
    Under WiFi advanced page the supplicant and driver debug levels are now
    user configurable

---
+++ php/setAdvanced.php
@@ -0,0 +1,21 @@
+<?php
+	require("../lrd_php_sdk.php");
+	if(!extension_loaded('lrd_php_sdk')){
+		syslog(LOG_WARNING, "ERROR: failed to load lrd_php_sdk");
+	}
+	header("Content-Type: application/json");
+	$advanced = json_decode(stripslashes(file_get_contents("php://input")));
+
+	$returnedResult = [
+		'SDCERR' => SDCERR_FAIL,
+	];
+
+	$result = LRD_WF_SetSuppLogLevel($advanced->{'suppDebugLevel'});
+	if ($result == SDCERR_SUCCESS){
+			$result = LRD_WF_Driver_set_debug($advanced->{'driverDebugLevel'}, null);
+	}
+
+	$returnedResult['SDCERR'] = $result;
+
+	echo json_encode($returnedResult);
+?>
