2e50a869c2a996957b1a085450f4651405df73ca
Joe Conley <joe.conley@lairdtech.com>
2016-06-08 16:16:08 -0400

    WebLCM: Add ability to rename a WiFi profile
    Added ability to rename a WiFi profile.
    Moved the ability to remove a profile to exist under edit profiles section.

---
+++ php/renameProfile.php
@@ -0,0 +1,27 @@
+<?php
+	require("../lrd_php_sdk.php");
+	if(!extension_loaded('lrd_php_sdk')){
+		syslog(LOG_WARNING, "ERROR: failed to load lrd_php_sdk");
+	}
+	header("Content-Type: application/json");
+	$Profile = json_decode(stripslashes(file_get_contents("php://input")));
+
+	$returnedResult = [
+		'SDCERR' => SDCERR_FAIL,
+	];
+
+	$cfgs = new SDCConfig();
+	$result = GetConfig($Profile->{'currentName'},$cfgs);
+
+	$cfgs->configName = $Profile->{'newName'};
+
+	if ($result == SDCERR_SUCCESS){
+		$result = ModifyConfig(trim($Profile->{'currentName'}),$cfgs);
+	}
+
+	$returnedResult['SDCERR'] = $result;
+
+	echo json_encode($returnedResult);
+
+	free_SDCConfig($cfgs);
+?>
