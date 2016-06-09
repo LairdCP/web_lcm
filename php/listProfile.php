c1c21a4976a167146e64e89dd1cf942368871820
Joe Conley <joe.conley@lairdtech.com>
2016-06-09 16:10:14 -0400

    WebLCM: Show auto-profile list when auto-profile is enabled
    When auto-profile is enabled a table is shown listing all profiles and their
    state in regards to auto-profile.

--- php/listProfile.php
+++ php/listProfile.php
@@ -9,29 +9,49 @@
 		'SDCERR' => SDCERR_FAIL,
 	];
 
-	$Count = new_ulongp();
-	$currentConfig = str_repeat(" ",CONFIG_NAME_SZ);
-	$result = GetNumConfigs($Count);
-
+	$rcs = new_RADIOCHIPSETp();
+	$result = LRD_WF_GetRadioChipSet($rcs);
 	if($result == SDCERR_SUCCESS){
-		$cfgs = new_SDCConfig_array(ulongp_value($Count));
-		$result = GetAllConfigs($cfgs, $Count);
+		$gcfgs = new SDCGlobalConfig();
+		$result = GetGlobalSettings($gcfgs);
 		if($result == SDCERR_SUCCESS){
-			$result = GetCurrentConfig(NULL, $currentConfig);
-			$returnedResult['currentConfig'] = trim($currentConfig);
-			for($i = 0;$i < ulongp_value($Count);$i++) {
-				$singleCFG = lrd_php_sdk::SDCConfig_array_getitem($cfgs,$i);
-				$profileList[$i] = $singleCFG->configName;
+			$Count = new_ulongp();
+			$currentConfig = str_repeat(" ",CONFIG_NAME_SZ);
+			$result = GetNumConfigs($Count);
+
+			if($result == SDCERR_SUCCESS){
+				$cfgs = new_SDCConfig_array(ulongp_value($Count));
+				$result = GetAllConfigs($cfgs, $Count);
+				if($result == SDCERR_SUCCESS){
+					$result = GetCurrentConfig(NULL, $currentConfig);
+					$returnedResult['currentConfig'] = trim($currentConfig);
+					$nBit = 1;
+					for($i = 0;$i < ulongp_value($Count);$i++) {
+						$singleCFG = lrd_php_sdk::SDCConfig_array_getitem($cfgs,$i);
+						$profileList[$i] = $singleCFG->configName;
+
+						$nBit = $nBit << 1;
+						if (($gcfgs->autoProfile & $nBit) == $nBit){
+							$autoProfileList[$singleCFG->configName] = true;
+						}else{
+							$autoProfileList[$singleCFG->configName] = false;
+						}
+					}
+				}
+				delete_SDCConfig_array($cfgs);
 			}
 		}
-		delete_SDCConfig_array($cfgs);
 	}
 	$returnedResult['NumConfigs'] = ulongp_value($Count);
 	$returnedResult['profiles'] = $profileList;
+	if (($gcfgs->autoProfile & ~1) or ($gcfgs->autoProfile & 1)){
+		$returnedResult['autoProfiles'] = $autoProfileList;
+	}
 	$returnedResult['SDCERR'] = $result;
 
 	echo json_encode($returnedResult);
 
+	delete_RADIOCHIPSETp($rcs);
 	delete_ulongp($Count);
 
 ?>
