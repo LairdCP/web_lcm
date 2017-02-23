56c0a929cb59dc954e5fa83ae01193aeae1c9017
Joe Conley <joe.conley@lairdtech.com>
2017-02-23 10:58:07 -0500

    WebLCM: Fix bridging corner cases
    Switching NAT to different interface is now fixed.
    Stopping a bridge no longer deletes bridge interface, only disables it.
    
    bug 10782

--- plugins/interfaces/php/setSpecialInterfaces.php
+++ plugins/interfaces/php/setSpecialInterfaces.php
@@ -13,16 +13,17 @@
 	//IPv4
 	switch (strtolower($InterfaceData->IPv4->{'state'})){
 		case "none":
-			$result = LRD_ENI_DisableInterface("br0");
+			$result = LRD_ENI_AutoStartOff("br0");
 			if ($result == SDCERR_SUCCESS){
-				$result = LRD_ENI_AutoStartOff("br0");
-				if ($result == SDCERR_SUCCESS){
-					exec("/usr/sbin/ifrc br0 stop");
-					if ($InterfaceData->IPv4->{'previousNAT'}){
-						exec("/usr/sbin/ifrc " . escapeshellcmd($InterfaceData->IPv4->{'int_2'}) . " restart");
-						$result = LRD_ENI_DisableNat($InterfaceData->IPv4->{'int_2'});
-					}
+				// Be safe and stop br0
+				exec("/usr/sbin/ifrc br0 stop");
+				if ($InterfaceData->IPv4->{'previousNAT'}){
+					// Clear previous NATs
+					$result = LRD_ENI_DisableNat($InterfaceData->IPv4->{'previousNAT'});
 				}
+				// Put both interfaces in clean state
+				exec("/usr/sbin/ifrc " . escapeshellcmd($InterfaceData->IPv4->{'int_1'}) . " restart");
+				exec("/usr/sbin/ifrc " . escapeshellcmd($InterfaceData->IPv4->{'int_2'}) . " restart");
 			}
 			break;
 		case "bridge":
@@ -33,33 +34,37 @@
 					$result = LRD_ENI_SetBridgePorts("br0", $InterfaceData->IPv4->{'int_1'} . " " . $InterfaceData->IPv4->{'int_2'});
 					if ($result == SDCERR_SUCCESS){
 						if ($InterfaceData->IPv4->{'previousNAT'}){
-							$result = LRD_ENI_DisableNat($InterfaceData->IPv4->{'int_2'});
-							exec("/usr/sbin/ifrc br0 restart", $output, $result);
+							// Clear previous NATs
+							$result = LRD_ENI_DisableNat($InterfaceData->IPv4->{'previousNAT'});
 						}
+						// Enable bridge, handles starting of bridged interfaces
+						exec("/usr/sbin/ifrc br0 restart", $output, $result);
 					}
 				}
 			}
 			break;
 		case "nat":
-			$result = LRD_ENI_DisableInterface("br0");
+			$result = LRD_ENI_AutoStartOff("br0");
 			if ($result == SDCERR_SUCCESS){
-				$result = LRD_ENI_AutoStartOff("br0");
+				// Be safe and stop br0
+				exec("/usr/sbin/ifrc br0 stop");
+				$result = LRD_ENI_AddInterface($InterfaceData->IPv4->{'int_2'});
 				if ($result == SDCERR_SUCCESS){
-					$result = LRD_ENI_AddInterface($InterfaceData->IPv4->{'int_2'});
+					$result = LRD_ENI_EnableInterface($InterfaceData->IPv4->{'int_2'});
 					if ($result == SDCERR_SUCCESS){
-						$result = LRD_ENI_EnableInterface($InterfaceData->IPv4->{'int_2'});
+						$result = LRD_ENI_AutoStartOn($InterfaceData->IPv4->{'int_1'});
 						if ($result == SDCERR_SUCCESS){
-							$result = LRD_ENI_AutoStartOn($InterfaceData->IPv4->{'int_1'});
+							$result = LRD_ENI_AutoStartOn($InterfaceData->IPv4->{'int_2'});
 							if ($result == SDCERR_SUCCESS){
-								$result = LRD_ENI_AutoStartOn($InterfaceData->IPv4->{'int_2'});
+								if ($InterfaceData->IPv4->{'previousNAT'}){
+									// Clear previous NATs
+									$result = LRD_ENI_DisableNat($InterfaceData->IPv4->{'previousNAT'});
+								}
+								$result = LRD_ENI_EnableNat($InterfaceData->IPv4->{'int_2'});
 								if ($result == SDCERR_SUCCESS){
-									if ($InterfaceData->IPv4->{'previousNAT'}){
-										$result = LRD_ENI_DisableNat($InterfaceData->IPv4->{'int_2'});
-										if ($result != SDCERR_SUCCESS){
-											break;
-										}
-									}
-									$result = LRD_ENI_EnableNat($InterfaceData->IPv4->{'int_2'});
+									// Make sure int 1/wlan0 is started
+									exec("/usr/sbin/ifrc " . escapeshellcmd($InterfaceData->IPv4->{'int_1'}) . " start", $output, $result);
+									// Restart int 2 to get desired state
 									exec("/usr/sbin/ifrc " . escapeshellcmd($InterfaceData->IPv4->{'int_2'}) . " restart", $output, $result);
 								}
 							}
@@ -77,8 +82,10 @@
 		switch (strtolower($InterfaceData->IPv6->{'state'})){
 			case "none":
 				if ($InterfaceData->IPv6->{'previousNAT'}){
+					// Clear previous NATs
+					$result = LRD_ENI_DisableNat6($InterfaceData->IPv6->{'previousNAT'});
+					// Put interface in clean state
 					exec("/usr/sbin/ifrc " . escapeshellcmd($InterfaceData->IPv6->{'int_2'}) . " restart");
-					$result = LRD_ENI_DisableNat6($InterfaceData->IPv6->{'int_2'});
 				}
 				break;
 			case "nat":
@@ -91,12 +98,11 @@
 							$result = LRD_ENI_AutoStartOn($InterfaceData->IPv6->{'int_2'});
 							if ($result == SDCERR_SUCCESS){
 								if ($InterfaceData->IPv6->{'previousNAT'}){
-									$result = LRD_ENI_DisableNat6($InterfaceData->IPv6->{'int_2'});
-									if ($result != SDCERR_SUCCESS){
-										break;
-									}
+									// Clear previous NATs
+									$result = LRD_ENI_DisableNat6($InterfaceData->IPv6->{'previousNAT'});
 								}
 								$result = LRD_ENI_EnableNat6($InterfaceData->IPv6->{'int_2'});
+								// Restart int 2 to get desired state
 								exec("/usr/sbin/ifrc " . escapeshellcmd($InterfaceData->IPv6->{'int_2'}) . " restart", $output, $result);
 							}
 						}
