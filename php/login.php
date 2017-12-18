05dc7f292f528153b19820571ea5c68439343fc9
Joe Conley <joe.conley@lairdtech.com>
2017-12-18 10:16:50 -0500

    Add rate limiter to login
    The limiter will allow 3 failed logins then a 30 second lockout comes
    into effect. During this lockout no login attempt is accepted.
    On a successful login the rate limiter is reset.
    A power cycle also resets the rate limiter.
    Max login attempts before lockout and lockout timer are both configurable
    via webLCM.ini file.
    
    bug 12409

--- php/login.php
+++ php/login.php
@@ -8,6 +8,8 @@
 
 	$returnedResult = [
 		'SDCERR' => SDCERR_FAIL,
+		'LOCKED' => false,
+		'TIME_LEFT' => 0,
 	];
 
 	if (!isset($_SESSION['passwordFile'])){
@@ -21,12 +23,41 @@
 		}
 		$returnedResult['SDCERR'] = $genCreds;
 	}
-	if (password_verify( $creds->{'username'},trim($lines[0])) == true && password_verify($creds->{'password'},trim($lines[1])) == true){
-		$returnedResult['SDCERR'] = SDCERR_SUCCESS;
-		if (!isset($_SESSION['LAST_ACTIVITY'])){
-			$_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
+
+	// Find out how long we have been locked
+	$timeLocked = time() - $_SESSION['LOCKED_START_TIME'];
+
+	// Unlock if we have waited long enough
+	if (isset($_SESSION['LOCKED_START_TIME']) && $_SESSION['FAIL_LOGIN_COUNTER'] >= $_SESSION['maxLoginAttempts'] && $timeLocked > $_SESSION['lockedOutTimeout']){
+		// Unlocking system
+		$_SESSION['LOCKED'] = false;
+		$_SESSION['FAIL_LOGIN_COUNTER'] = 0;
+	}
+
+	if (!$_SESSION['LOCKED']){
+		if (password_verify( $creds->{'username'},trim($lines[0])) == true && password_verify($creds->{'password'},trim($lines[1])) == true){
+			$returnedResult['SDCERR'] = SDCERR_SUCCESS;
+			// Update last activity time stamp
+			$_SESSION['LAST_ACTIVITY'] = time();
+		} else {
+			$_SESSION['LOCKED_START_TIME'] = time();
 		}
 	}
+
+	if ($returnedResult['SDCERR'] != SDCERR_SUCCESS){
+		$_SESSION['FAIL_LOGIN_COUNTER'] += 1;
+	}
+
+	if ($_SESSION['FAIL_LOGIN_COUNTER'] >= $_SESSION['maxLoginAttempts']){
+		// Locking system
+		$_SESSION['LOCKED'] = true;
+		$returnedResult['LOCKED'] = true;
+		if (!isset($_SESSION['LOCKED_START_TIME'])){
+			$_SESSION['LOCKED_START_TIME'] = time();
+		}
+	}
+
+	$returnedResult['TIME_LEFT'] = $_SESSION['lockedOutTimeout'] - $timeLocked;
 
 	echo json_encode($returnedResult);
 ?>
